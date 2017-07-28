c137502e2784403f15d4818815f2d2784862d92b
Jacopo Libè <jacopo.libe@gmail.com>
2017-07-28 14:23:42 +0200

    Aggiunto metodo per leggere stringhe di lunghezza indefinita (con massimo MAX_PARAMS parametri)

--- main.c
+++ main.c
@@ -1,16 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
-//Per ora pieno di merda, prima o poi separerò il tutto
-int main()
+#define MAX_PARAMS 3
+
+int readCommand(char** comando)
 {
-  char* comando;
-  char* data;
-  int scanResult = 0;
+  char* str = (char*)malloc(sizeof(char));
+  int c;
+  int len = 0;
+  int clen = 0;
+  while((c = getchar()) != '\n' && c != EOF)
+  {
+    //Se non ho uno spazio tra caratteri leggo la stringa
+    if(c != ' ')
+    {
+      str[len++] = c;
+      str = (char*)realloc(str, (len + 1) * sizeof(char));
+    }
+    else
+    {
+      //Se è uno spazio, allora sono nell'intermezzo tra più stringhe
+      //cerco di inserire i dati nell'array dei parametri (se c'è spazio)
+      if(clen < MAX_PARAMS)
+      {
+        comando[clen++] = str;
+      }
+      else
+      {
+        //Non c'è più spazio nei parametri, ignoro la stringa e libero la sua memoria
+        free(str);
+      }
+      str = (char*)malloc(sizeof(char)); //Reinstazio una nuova stringa, pronta per il prossimo parametro
+      len = 0;
+    }
+  }
 
+  //Prendo l'ultimo parametro prima di EOF
+  if(clen < MAX_PARAMS)
+  {
+    comando[clen++] = str;
+  }
+
+  return clen;
+}
+
+int main(void)
+{
+  char** comando = NULL;
+  int count = 0;
   do
   {
-      scanResult = scanf("%s %s", comando, data);
+    //Reistanzio comando
+    if(comando != NULL)
+      free(comando);
+    comando =  (char**)malloc(MAX_PARAMS * sizeof(char*));
 
-  } while(scanResult > 0 && !strcmp(comando, "exit"));
+    //Leggo il prossimo comando
+    count = readCommand(comando);
+  } while(count > 0 && strcmp(comando[0], "exit"));
   return -1;
 }
